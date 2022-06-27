8eb526cf6c004f210fd9f95d8dda8fc6b2587cfc
Ondion <fabio.xavier@live.com>
2022-06-27 13:12:02 -0300

    feat(JWT-stepBystep): adiciona exe com configuração do token

--- Node/Exercícios/JWB-stepBystep/index.js
+++ Node/Exercícios/JWB-stepBystep/index.js
@@ -4,7 +4,7 @@
 const jwt = require('jsonwebtoken');
 
 const secret = "senha";
-const fakeSecret = "Senha";
+const fakeSecret = "senha";
 
 const jwtConfig = {
   expiresIn: '7d',
@@ -16,16 +16,16 @@ user = {
   constantes: "const dentro do data/user"
 }
 
-fakeUser = {
-  valores: "valores dentro do data/user",
-  constantes: "const dentro do data/user"
-}
+// fakeUser = {
+//   valores: "valores dentro do data/user",
+//   constantes: "const dentro do data/user"
+// }
 
-
+fakeToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2YWxvcmVzIjoidmFsb3JlcyBkZW50cm8gZG8gZGF0YS91c2VyIiwiY29uc3RhbnRlcyI6ImNvbnN0IGRlbnRybyBkbyBkYXRhL3VzZXIiLCJpYXQiOjE2NTYzNDYxNjEsImV4cCI6MTY1Njk1MDk2MX0.JRrjKycJrfHf390iSJ25B_HGzJ3qBdVz8Nek2Cu8V4E"
 
 const token = jwt.sign({ ...user }, secret, jwtConfig);
 
-const fakeToken = jwt.sign({ ...fakeUser }, fakeSecret, jwtConfig);
+// const fakeToken = jwt.sign({ ...fakeUser }, fakeSecret, jwtConfig);
 
 const decoded = jwt.verify(fakeToken, secret);
 
