6c7d1d537a6297e6da11849bdd5d8d87f5ef9213
Ondion <fabio.xavier@live.com>
2022-07-08 16:19:55 -0300

    feat: adiciona conteúdo typescript didatico

--- TypeScript/exercícios/calc_area_TS/index.ts
+++ TypeScript/exercícios/calc_area_TS/index.ts
@@ -1,4 +1,22 @@
-class tete {
-  name: string = 'teste',
-  idade: number = 12,
+enum stages {
+  inverno,
+  primavera,
+  verão,
+  outono
 }
+
+enum year {
+  JANEIRO = "Janeiro",
+  FEVEREIRO = "Fevereiro",
+  MARCO = "Março",
+  ABRIL = "Abril",
+  MAIO = "Maio",
+  JUNHO = "Junho",
+  JULHO = "Julho",
+  AGOSTO = "Agosto",
+  SETEMBRO = "Setembro",
+  OUTUBRO = "Outubro",
+  NOVEMBRO = "Novembro",
+  DEZEMBRO = "Dezembro",
+}
+
