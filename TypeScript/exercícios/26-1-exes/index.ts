f8e62f61bebca13a312d2b5d2fdb631f2ae23112
Ondion <fabio.xavier@live.com>
2022-07-09 23:55:52 -0300

    feat: conteúdo exercícios typescript

---
+++ TypeScript/exercícios/26-1-exes/index.ts
@@ -0,0 +1,27 @@
+enum meses {
+  janeiro = 'janeiro',
+  fevereiro = 'fevereiro',
+  março = 'março',
+  abril = 'abril',
+  maio = 'maio',
+  junho = 'junho',
+  julho = 'julho',
+  agosto = 'agosto',
+  setembro = 'setembro',
+  outubro = 'outubro',
+  novembro = 'novembro',
+  dezembro = 'dezembro',
+}
+
+enum estacoes {
+  outono = 'outono',
+  inverno = 'inverno',
+  primavera = 'primavera',
+  verao = 'verao',
+}
+
+// converte em metros;
+
+function convert(valor: number, unidade: string, base: string): string {
+  return 'ola'
+};
